c8d0dd1c591d95e43b0508d6a3ea487086b4905f
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-07-02 00:29:47 +0530

    detectcycle node

--- LL.java
+++ LL.java
@@ -69,6 +69,28 @@ public class LL {
         }
         return false;
     }
+    public Node detectCycleNode(Node node){
+        Node slow = head;
+        Node fast = head;
+        // int count = 0;
+       while(fast!=null && fast.next != null){
+           
+          
+           fast = fast.next.next;
+           slow = slow.next;
+           if(slow == fast){
+              slow = head;
+              while(slow!=fast){
+                  slow= slow.next;
+                  fast= fast.next;
+              }
+              return slow;
+           }
+         
+           
+       }
+       return null;
+    }
 
     public int deleteFirst() {
         int val = head.value;
